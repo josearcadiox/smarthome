32d88236f7a293a624f1cb3c96102468d76f3f7c
josearcadiox <josearcadioyt@gmail.com>
2025-05-22 21:49:00 -0700

    Navegación de pantallas principales de aplicación

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,61 @@
+// main.dart
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(SmartHomeApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class SmartHomeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Smart Control',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: Color(0xFF1A1A2E),
+        cardColor: Color(0xFF16213E),
+        colorScheme: ColorScheme.dark(
+          primary: Color(0xFF0F4C75),
+          secondary: Color(0xFF3282B8),
+          surface: Color(0xFF16213E),
+        ),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Color(0xFF16213E),
+        selectedItemColor: Color(0xFF3282B8),
+        unselectedItemColor: Colors.grey,
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Inicio'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.devices),
+            label: 'Dispositivos',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.energy_savings_leaf),
+            label: 'Energía',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Ajustes'),
+        ],
       ),
     );
   }
